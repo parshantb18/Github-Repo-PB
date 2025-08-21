ce80667d041927d341689c10aaa6f9a8608a4e80
Parshant Bansal <pbansal1_be24@thapar.edu>
2025-08-22 01:24:36 +0530

    Q2

---
+++ DS/Assignment-2/Additional Q's/Q2.cpp
@@ -0,0 +1,4 @@
+/* String Split Challenge 
+You are given a string consisting of lowercase English alphabets. Your task is to determine if 
+it's possible to split this string into three non-empty parts (substrings) where one of these 
+parts is a substring of both remaining parts*/
