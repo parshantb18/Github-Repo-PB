a3ad6761eb0700857919312688c81df1fb1edf1b
Parshant Bansal <pbansal1_be24@thapar.edu>
2025-08-22 12:51:16 +0530

    updated Q5

--- DS/Assignment-2/Additional Q's/Q5.cpp
+++ DS/Assignment-2/Additional Q's/Q5.cpp
@@ -3,29 +3,44 @@ remaining elements to the right.
 Note that elements beyond the length of the original array are not written. Do the above 
 modifications to the input array in place and do not return anything.*/
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n,arr[100],a[101];
-    cout<<"Enter the no. of elements in your array:";
-    cin>>n;
-    cout<<"Enter the elements of your array:"<<endl;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    for(int i=n;i>0;i--){
-        for(int j=i-1;j>=0;j--){
-            if(arr[j]==2){
-                a[i-1]=a
-            }
-            a[i]=arr[j];
-            
+
+    
+    for (int i = n - 1; i >= 0; i--) {
+        if (arr[i] == 2) {
+            if (i + 1 < n){ 
+                arr[i + 1] = 2;
         }
     }
-   
+}
 
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] == 2 && arr[i + 1] != 2) {
+            for (int j = n - 1; j > i + 1; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[i + 1] = 2;
+            i++;
+        }
+    }
 
-
+    cout << "Modified array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
