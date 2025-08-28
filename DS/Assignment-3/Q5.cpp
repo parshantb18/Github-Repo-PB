591e971af7925d9b884eda70b71407417bd20d12
Parshant Bansal <pbansal1_be24@thapar.edu>
2025-08-28 23:26:50 +0530

    ///

--- DS/Assignment-3/Q5.cpp
+++ DS/Assignment-3/Q5.cpp
@@ -1,35 +1,67 @@
 /*Write a program for the evaluation of a Postfix expression.*/
 #include <iostream>
-#include <stack>
 #include <string>
 using namespace std;
 
-int main()
-{
+class Stack {
+private:
+    int arr[100];
+    int topIndex;
+
+public:
+    Stack() {
+        topIndex = -1;
+    }
+
+    void push(int val) {
+        if (topIndex >= 99) {
+            cout << "Stack Overflow!" << endl;
+            return;
+        }
+        arr[++topIndex] = val;
+    }
+
+    void pop() {
+        if (topIndex < 0) {
+            cout << "Stack Underflow!" << endl;
+            return;
+        }
+        topIndex--;
+    }
+
+    int top() {
+        if (topIndex < 0) {
+            cout << "Stack Empty!" << endl;
+            return -1;
+        }
+        return arr[topIndex];
+    }
+
+    bool empty() {
+        return topIndex == -1;
+    }
+};
+
+int main() {
     string exp;
     cout << "Enter postfix expression: ";
     cin >> exp;
 
-    stack<int> st;
+    Stack st;
 
-    for (int i = 0; i < exp.length(); i++)
-    {
+    for (int i = 0; i < exp.length(); i++) {
         char c = exp[i];
 
-        // if operand, push to stack
-        if (isdigit(c))
-        {
-            st.push(c - '0'); // convert char to int
+        if (isdigit(c)) {
+            st.push(c - '0');
         }
-        else
-        { // operator
+        else {
             int b = st.top();
             st.pop();
             int a = st.top();
             st.pop();
 
-            switch (c)
-            {
+            switch (c) {
             case '+':
                 st.push(a + b);
                 break;
@@ -42,6 +74,8 @@ int main()
             case '/':
                 st.push(a / b);
                 break;
+            default:
+                cout << "Invalid operator: " << c << endl;
             }
         }
     }
