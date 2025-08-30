8fc9748f30a8a8f0b593266612dcc39e3331c62c
Parshant Bansal <pbansal1_be24@thapar.edu>
2025-08-30 20:44:01 +0530

    added Q2

---
+++ OOPS/3rd Assignment/Q3.cpp
@@ -0,0 +1,9 @@
+/*Verify the following about destructor by writing the program:
+ (i)Name should begin with tilde sign(~) and must match class name.
+ (ii)There cannot be more than one destructor in a class.
+ (iii)Destructors do not allow any parameter.
+ (iv)They do not have any return type, just like constructors.
+ 
+When you do not specify any destructor in a class, compiler generates a default 
+destructor and inserts it into your code.*/
+
