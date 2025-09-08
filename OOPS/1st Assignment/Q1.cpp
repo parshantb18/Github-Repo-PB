e5486e709d89e8c6e40c9f1cd5c6bdabc5827e69
Parshant Bansal <pbansal1_be24@thapar.edu>
2025-09-08 11:04:15 +0530

    updated errors

--- OOPS/1st Assignment/Q1.cpp
+++ OOPS/1st Assignment/Q1.cpp
@@ -1,11 +1,13 @@
-/*Write a program (WAP) to display "Hello World" on console display. WAP to 
-implement the following control characters: 
-‘\n’ is for new line, or you can use endl – cout<<endl<<“message”; 
-‘\t’ is for tab ; ‘\a’ is an alarm sound; ‘\r’ is carriage return to go to the beginning of 
+/*Write a program (WAP) to display "Hello World" on console display. WAP to
+implement the following control characters:
+‘\n’ is for new line, or you can use endl – cout<<endl<<“message”;
+‘\t’ is for tab ; ‘\a’ is an alarm sound; ‘\r’ is carriage return to go to the beginning of
 the current line*/
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"\tINFO\nMy name is xyz\nI reside at abc"<<endl;
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "\tINFO\nMy name is xyz\nI reside at abc" << endl;
     return 0;
 }
