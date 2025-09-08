aed6eb45324637c83a85929f85f33c63e528f0a5
Parshant Bansal <pbansal1_be24@thapar.edu>
2025-09-08 11:05:10 +0530

    fck

--- OOPS/1st Assignment/Q2.cpp
+++ OOPS/1st Assignment/Q2.cpp
@@ -1,15 +1,16 @@
-/*Write a C++ program that will ask for a temperature in Celsius and display it in 
+/*Write a C++ program that will ask for a temperature in Celsius and display it in
 degree Fahrenheit.[F=9C/5+32] */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int C,F;
-    cout<<"Enter the temperzture in celsius:";
-    cin>>C;
+int main()
+{
+    int C, F;
+    cout << "Enter the temperature in celsius:";
+    cin >> C;
 
-    F = (9*C/5) + 32;
+    F = (9 * C / 5) + 32;
 
-    cout<<"The resultant temperature in fahrenheit is:"<<F<<endl;
+    cout << "The resultant temperature in fahrenheit is:" << F << endl;
     return 0;
 }
