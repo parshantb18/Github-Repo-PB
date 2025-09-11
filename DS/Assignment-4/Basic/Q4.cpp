876f9f54b966b95618be19fd1d687d1113dfc8e7
Parshant Bansal <pbansal1_be24@thapar.edu>
2025-09-12 01:18:32 +0530

    added assign 4 basic

---
+++ DS/Assignment-4/Basic/Q4.cpp
@@ -0,0 +1,53 @@
+/*Write a program to find first non-repeating character in a string using Queue. Sample I/P: a a
+b c Sample O/P: a -1 b b*/
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+void firstNonRepeating(string str)
+{
+    unordered_map<char, int> freq; // frequency map
+    queue<char> q;                 // queue to store characters
+
+    for (char ch : str)
+    {
+        if (ch == ' ')
+            continue; // skip spaces if input has them
+
+        // update frequency
+        freq[ch]++;
+
+        // push into queue
+        q.push(ch);
+
+        // remove all characters from front that are repeating
+        while (!q.empty() && freq[q.front()] > 1)
+        {
+            q.pop();
+        }
+
+        // print first non-repeating
+        if (q.empty())
+        {
+            cout << -1 << " ";
+        }
+        else
+        {
+            cout << q.front() << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string: ";
+    getline(cin, str);
+
+    cout << "Output: ";
+    firstNonRepeating(str);
+
+    return 0;
+}
