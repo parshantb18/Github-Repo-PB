135cc1b2cfd0830c5fcd3411a68a1619505a0eb7
Parshant Bansal <pbansal1_be24@thapar.edu>
2025-11-28 10:07:41 +0530

    .

--- DS/Assignment-4/Additional/Q2.cpp
+++ DS/Assignment-4/Additional/Q2.cpp
@@ -1,6 +1,6 @@
 /*Given a queue with random elements, we need to sort it. We are not allowed to use extra space. The
 operations allowed on queue are:
-       1.   enqueue() : Adds an item to rear of queue.
+1. enqueue() : Adds an item to rear of queue.
 2. dequeue() : Removes an item from front of queue.
 3. isEmpty() : Checks if a queue is empty.
 Input: 11, 5, 4, 21
